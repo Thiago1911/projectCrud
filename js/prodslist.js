78a38f97476c3b2d278312b38fd36a05e6b41c04
Thiago de Paula Fernandes <thiagofernandes1911@gmail.com>
2021-05-09 16:04:14 -0300

    Construção da tela de consulta de produtos

--- js/prodslist.js
+++ js/prodslist.js
@@ -8,3 +8,27 @@ const tbodyList = document.getElementById('tbodyList');
 const api =  axios.create({
     baseURL: 'http://18.224.8.119:3334/'
 })
+
+function listAll(){
+    console.log('Consulta de dados ....');
+    api.get('produtos').then(res=>{
+        console.log('Realizando a consulta ....');
+        const data = res.data;
+        let i, tr;
+
+        for(i=0;i<data.length;i++){
+            tr = '<tr>' + 
+                    '<td>' + data[i].cod + '/<td>' +
+                    '<td>' + data[i].nome + '/<td>' +
+                    '<td>' + data[i].descri + '/<td>' +
+                    '<td>' + data[i].qtda + '/<td>' +
+                    '<td>' + data[i].fabricante + '/<td>' +
+                    '<td>' + data[i].datahora + '/<td>' +
+                 '/<tr>';
+            tbodyList.innerHTML += tr;
+            tr = tbodyList.childNodes;
+        }
+    });
+}
+
+listAll();
